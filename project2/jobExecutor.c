e82a3c5df7eba60931241d56837a4fe58769af99
“IOANNA <sdi1400091@di.uoa.gr>
2018-04-29 20:37:28 +0300

    lol

--- project2/jobExecutor.c
+++ project2/jobExecutor.c
@@ -38,7 +38,7 @@ int main(int argc,char* argv[]){
   int ch=0, lines=0, ret;
   size_t size= 1024;
 
-////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
   FILE *fp = fopen(doc, "r");
       if (fp == NULL){
           fprintf(stderr,"Error opening file\n");
@@ -55,12 +55,11 @@ int main(int argc,char* argv[]){
   char **array;
   array = malloc(sizeof(char*)*lines);    //just to save the lines
 
-  fseek(fp, 0, SEEK_SET); //go to the top of file
+  fseek(fp, 0, SEEK_SET);                 //go to the top of file
 
   for (i=0; i<lines; i++){
     ret = getline(&buffer, &size, fp);
-    //while(ret>-1) {
-      if (ret<0)                            //if line ended
+      if (ret<0)                          //if line ended
         break;
 
       buffer[ret-1]= '\0' ;
@@ -69,12 +68,12 @@ int main(int argc,char* argv[]){
 
   }
   fclose(fp);
-////////////////////////////////////////////////////////////////////////
-numdocs= lines/w;     // posa paths tha parei o kathenas
+////////////////////////////////////////////////////////////////////////////////
+numdocs= lines/w;                         // posa paths tha parei o kathenas
 if (lines%w != 0){
-  docsdiv= lines%w;   //an perisseuei kapoio to pairnei o prwtos
+  docsdiv= lines%w;                       //an perisseuoun
 }
-////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 if(numdocs == 0){
     printf("Some kids does not take paths (num of kids:)%d\n",(w-lines) );
@@ -84,7 +83,7 @@ if(numdocs == 0){
   char Input_parent[24];
   char Output_parent[24];
 
-  /* Start CHILDREN. */
+// Start CHILDREN. //
   for (i = 0; i < w; i++) {
     sprintf(Input_parent, "Input%d", i);
     sprintf(Output_parent,"Output%d",i);
@@ -104,16 +103,16 @@ if(numdocs == 0){
       exit(0);
 
     } else if (pids[i] == 0){
-////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 //CHILD//
         char msgbuf[BUFSIZE];
         int in,out,child_numdocs=numdocs,id=i, linestxt=0;
+        int cwords=0, clines=0,cbyte=0;
         if(id < docsdiv){
           child_numdocs++;
         }
         if(child_numdocs==0)return 0;
-        printf("i am child %d  with docs %d\n",id,child_numdocs);
         char** my_paths;
         my_paths = malloc(sizeof(char*)*child_numdocs);
 
@@ -132,7 +131,6 @@ if(numdocs == 0){
 
         for(i = 0 ; i < child_numdocs ; i++){    //gia osa paths prepei na diavasei
           while (read ( in , msgbuf , BUFSIZE +1) <=0);
-          printf("PATH :%s  %d \n",msgbuf,id );
           my_paths[i] = malloc(sizeof(char)*strlen(msgbuf)+1);
           strcpy(my_paths[i],msgbuf);
           write(out,"NextPath",strlen("NextPath")+1);
@@ -147,7 +145,6 @@ if(numdocs == 0){
             while ((dir = readdir(d)) != NULL) {
               if (strcmp(dir->d_name,".") && strcmp(dir->d_name,"..")){
                 strcpy(txtname, dir->d_name);
-                printf("TXT NAME: %s\n", txtname);
 
                 /////edo prospathw na anoiksw ta arxeiakia/////
                 char* pathtxt;
@@ -166,33 +163,30 @@ if(numdocs == 0){
                   ch2 = fgetc(fp2);
                   if(ch2 == '\n'){
                     linestxt++;
+                    clines++;
                   }
                 }
                 //GIA KATHE ARXEIAKI//
-                printf("LINES %d\n",linestxt);
 
                 char **arraytxt, *token;
                 int ret2, k;
                 arraytxt = malloc(sizeof(char*)*linestxt);    //just to save the lines
 
-                fseek(fp2, 0, SEEK_SET); //go to the top of file
+                fseek(fp2, 0, SEEK_SET);                      //go to the top of file
                 for(k=0; k<linestxt; k++){
                   ret2 = getline(&buffer, &size, fp2);
-                    if (ret2<0)                            //if line ended
+                    if (ret2<0)                               //if line ended
                       break;
 
                     buffer[ret2-1]= '\0' ;
                     arraytxt[k] = malloc(sizeof(char )* strlen(buffer)+1);
                     strcpy(arraytxt[k], buffer);
-                    //printf("DES EDO %s\n",arraytxt[k]);
 
                     token= strtok(arraytxt[k]," ");
-                    //printf("TOKEN1: %s\n", token);
                     while (token != NULL){
-                      //printf("TOKEN %s\n",token);
-                      //INSERT TRIE KTL
                       insertTrie(trie, token,pathtxt, (k+1));
-
+                      cwords++;
+                      cbyte+=strlen(token);
                       token= strtok(NULL," ");
                     }
                     free(arraytxt[k]);
@@ -206,8 +200,8 @@ if(numdocs == 0){
           }
         }
 
-        //printf("id %d    to while\n",id );
-        ///////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
         char *logtxt = malloc(sizeof(char)*strlen("./Log/Worker_.txt")+10);
         sprintf(logtxt,"./Log/Worker_%d.txt",id);
         FILE *f3 = fopen(logtxt, "w");
@@ -215,35 +209,53 @@ if(numdocs == 0){
           fprintf(stderr,"Error opening file f3\n");
           exit(-1);
         }
-        const char *text = "Write this to the file";
-        fprintf(f3, "Some text: %s\n", text);
-        fclose(f3);
-        ////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
         char* tempWord;
         char* token2, *words;
+        char strsend[128];
+        time_t t;
+        time(&t);
+        int ret3,k;
         words= malloc(sizeof(char)*size);
         token2= malloc(sizeof(char)*size);
+
         ListNode *result;
         while(1){
           if (  read ( in, msgbuf, BUFSIZE +1) > 0){
-              tempWord = strtok(msgbuf," ");
+            tempWord = strtok(msgbuf," ");
 
             if(!strcmp(tempWord,"/exit")){
               break;
           }else if(!strcmp(tempWord,"/search")){
+              FILE * fp2;
               tempWord = strtok(NULL, "");
               strcpy(words, tempWord);
               token2=strtok(words," ");
               while (token2!=NULL) {
-                printf("Mpika sto search\n");
                 result= searchTrie(trie, token2);
                 while(result!=NULL){
-                    printf("ID %d PATH %s \n",result->id, result->path);
+                    fprintf(f3, "%s : /search : %s : %s\n",ctime(&t), token2, result->path);
+                    fp2 = fopen(result->path,"r");
+                    for(k=1; k<=result->id; k++){
+                      ret3 = getline(&buffer, &size, fp2);
+                      if (ret3<0) break;
+                      if(k == result->id){
+                        printf("Word: %s Line: %d Path: %s Whole line: %s\n",token2,result->id,result->path,buffer);
+                        break;
+                      }
+                    }
+                    fclose(fp2);
                     result= result->next;
                 }
                 token2= strtok(NULL," ");
               }
               write(out,"SearchOk",strlen("SearchOk")+1);
+            }else if(!strcmp(tempWord,"/wc")){
+              //printf("Cwords %d Lines %d\n",cwords,clines);
+              sprintf(strsend,"%d-%d-%d\n", cwords, clines,cbyte);
+              write(out,strsend,strlen(strsend)+1);
+
+
             }
           }
         }
@@ -256,13 +268,14 @@ if(numdocs == 0){
         free(token2);
         free(logtxt);
         free(my_paths);
-        printf("child finished\n" );
+        fclose(f3);
+        //printf("child finished\n" );
         return 0;
       }
 ////////////////////////////////////////////////////////////////////////
 
 
-      //FATHER
+//FATHER
       //open the named pipes for each child
       if((in_father[i]=open(Input_parent,O_RDONLY|O_NONBLOCK))==-1){
           perror("Fail to open the named pipe_in\n");
@@ -302,7 +315,7 @@ if(numdocs == 0){
       tmpchild++;
     }
     while(1){
-      for(i = 0 ; i < docsdiv ; i++){        //kai edw apantoun ta paidia oti diavasan to upoloipa paths
+      for(i = 0 ; i < docsdiv ; i++){        //apantoun ta paidia
         if(read(in_father[i],msgbuf,BUFSIZE)>0){
           if(!strcmp(msgbuf,"NextPath")){
             counter_rec++;
@@ -318,11 +331,11 @@ if(numdocs == 0){
   }
   free(array);
 
-////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
   fp = stdin;
-  char* tempWord;
-  char* tempCommand;
-  char* test;
+  char* tempCommand,*tempWord;
+  char* test, *token3;
+  int Totalwords=0, Totallines=0,Totalbytes=0;
   while (1){
     ret = getline(&buffer, &size, fp);      //reading from stdin
     if (ret<0)break;
@@ -337,7 +350,7 @@ if(numdocs == 0){
 
     if(!strcmp(tempWord,"/search")){
       for( i=0; i<w; i++){
-        if (( nwrite = write ( out_father[i] ,buffer , strlen(buffer)+1) ) == -1){
+        if ((nwrite = write ( out_father[i] ,buffer , strlen(buffer)+1) ) == -1){
             perror ("Error in Writing 6");
             exit (2) ;
         }
@@ -354,10 +367,33 @@ if(numdocs == 0){
           if(counter_rec == w)break;
       }
 
+    }else if(!strcmp(buffer,"/wc")){
+      for(i=0 ; i<w ; i++){
+        if (( nwrite = write ( out_father[i] ,buffer , strlen(buffer)+1) ) == -1){
+            perror ("Error in Writing 7");
+            exit (2);
+        }
+      }
+      counter_rec= 0 ;
+      while (1) {
+        for(i = 0 ; i < w ; i++){
+          if(read(in_father[i],msgbuf,BUFSIZE)>0){
+            counter_rec++;
+            token3 = strtok(msgbuf,"-");
+            Totallines += atoi(token3);
+            token3 = strtok(NULL,"-");
+            Totalwords += atoi(token3);
+            token3 = strtok(NULL,"-");
+            Totalbytes += atoi(token3);
+          }
+        }
+        if(counter_rec == w)break;
+      }
+      printf("Total lines: %d words: %d and Bytes: %d\n",Totallines,Totalwords,Totalbytes );
+      Totallines = Totalwords = Totalbytes = 0 ;
     }else if(!strcmp(buffer,"/exit")){
         free(tempCommand);
         for(i=0 ; i<w ; i++){
-            printf("give exit%d   %d\n",i ,w);
             if (( nwrite = write ( out_father[i] ,"/exit" , strlen("/exit")+1) ) == -1){
                 perror ("Error in Writing 5");
                 exit (2) ;
@@ -365,7 +401,7 @@ if(numdocs == 0){
         }
         break;
     }else{
-        printf("wrong command\n" );
+        printf("Wrong Command\n" );
         free(tempCommand);
         continue ;
     }
@@ -373,9 +409,9 @@ if(numdocs == 0){
   }
 
   int status;
-  /* Wait for children to exit. */
+  // Wait for children to exit. //
   for(i = 0  ; i < w ; i++){
-      printf("Waiting: %d\n",waitpid(pids[i],&status,0));
+      printf("Finished: %d\n",waitpid(pids[i],&status,0));
   }
 
   for(i = 0 ; i < w ; i++){
@@ -388,6 +424,6 @@ if(numdocs == 0){
   }
   free(doc);
   free(buffer);
-return 0;
+  return 0;
 
 }
